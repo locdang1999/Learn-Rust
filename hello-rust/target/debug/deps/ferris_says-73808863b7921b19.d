088e8a09040dc68d14b05157f398d35cc3d09f77
Dang Gia Loc <74612458+locdang1999@users.noreply.github.com>
2024-01-27 00:09:54 +0700

    Hello rust

---
+++ hello-rust/target/debug/deps/ferris_says-73808863b7921b19.d
@@ -0,0 +1,7 @@
+D:\Today-I-Learn\rust\hello-rust\target\debug\deps\libferris_says-73808863b7921b19.rmeta: C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ferris-says-0.3.1\src\lib.rs
+
+D:\Today-I-Learn\rust\hello-rust\target\debug\deps\libferris_says-73808863b7921b19.rlib: C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ferris-says-0.3.1\src\lib.rs
+
+D:\Today-I-Learn\rust\hello-rust\target\debug\deps\ferris_says-73808863b7921b19.d: C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ferris-says-0.3.1\src\lib.rs
+
+C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ferris-says-0.3.1\src\lib.rs:
