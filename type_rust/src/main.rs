386940783c93e7f56eee07410c0e03c646aae101
Dang Gia Loc <74612458+locdang1999@users.noreply.github.com>
2024-02-17 00:59:49 +0700

    Done type casting

--- type_rust/src/main.rs
+++ type_rust/src/main.rs
@@ -19,17 +19,41 @@ fn main() {
         * Scalar: lưu trữ đơn giá trị như Integer, Float, Char, Boolean, String
         * Compound: lưu trừ đa giá trị như Array, Tuple
     */
+
     // *** Scalar
     let test_string = "Rust learn";
     let test_boolean = true;
-    let test_integer = 2024;
+    let test_integer = 2024; // i32: integer 32bit --> số dương & âm
     let test_char = 'b';
     println!("{test_string}, {test_boolean}, {test_integer}, {test_char}");
+
     // *** Hằng số: const
     const PI: f32 = 3.14;
     println!("{PI}");
+
     // *** Compound
     // * Array: Tất cả các phần từ phải cùng kiểu dữ liệu
     let test_array = [1, 2, 3, 4];
     println!("{}", test_array[2]);
+
+    // *** Type-Casting: Ép kiểu dữ liệu -> Dùng từ khóa "as" và sẽ có các trường hợp ko thể ép kiểu -> chuyển từ type này thành type khác (convert type)
+    // định nghĩa kiểu dữ liệu trước tiếp cho biến
+    let qt = 100u16; // u16: integer 16bit --> nhưng chỉ tính số dương
+    let qw: u16 = 300;
+    println!("{qt} === {qw}");
+    // convert
+    let change_qt = qw as u8; // u8: maxlength: 256
+    println!("{qw} !== {change_qt}"); // Nếu khi convert type vượt quá giới hạn của kiểu dữ liệu thì Rust sẽ tự động lấy --> giá trị của biến ép - cho max length của kiểu dữ liệu được ép
+
+    let chartr = 'A';
+    let change_int = chartr as u8;
+    println!("{chartr} vs {change_int}");
+
+    let flg = false;
+    let change_int_flg = flg as u8;
+    println!("{flg} vs {change_int_flg}");
+
+    // case ko thể convert
+    let intg: u32 = 65;
+    // let change_char = intg as char; // only `u8` can be cast as `char`, not `u32` invalid cast
 }
