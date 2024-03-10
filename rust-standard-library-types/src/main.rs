45d6149122f13da8379a618d8e37cede0d133603
Dang Gia Loc <hahagioi169@gmail.com>
2024-03-10 22:35:41 +0700

    Rust Standard Library Types - 50% - Done Vector

--- rust-standard-library-types/src/main.rs
+++ rust-standard-library-types/src/main.rs
@@ -43,4 +43,60 @@ fn main() {
     }
     vect2.remove(1);
     println!("Remove phần tử theo vị trí index: {:?}", vect2);
+
+    /*
+     * Sử dụng Iterator
+     *  - iter(): Mượn (borrowing) mỗi phần tử của collection sau mỗi lần duyệt
+     *  - into_iter(): Duyệt đúng từng phần tử của collection -> collection sẽ bị "drop" sau khi duyệt xong
+     *  - iter_mut(): Mượn mỗi phần tử sau mỗi lần duyệt và có thể thay đổi giá trị của phần tử (mutable reference)
+     * *** Kết hợp với closure
+     */
+
+    // iter()
+    let colors: Vec<&str> = vec!["Red", "Yellow", "Green"];
+    for color in colors.iter() {
+        println!("iter: {}", color);
+    }
+
+    // Collection vẫn còn "tồn tại" sau khi dùng inter();
+    println!("colors sau iter() = {:?}", colors);
+
+    // into_iter()
+    for color in colors.into_iter() {
+        println!("into_iter: {}", color);
+    }
+    // Collection bị "drop" sau khi dùng into_iter();
+    // println!("colors = {:?}", colors);  // borrow of moved value
+
+    // iter_mut()
+    let mut colors_itmut: Vec<&str> = vec!["Red", "Yellow", "Green"];
+    for color in colors_itmut.iter_mut() {
+        println!("iter: {}", color);
+        *color = "Black";
+    }
+
+    // Collection vẫn còn "tồn tại" sau khi dùng và có thể thay đổi giá trị trong khi dùng iter_mut();
+    println!("colors sau iter() = {:?}", colors_itmut);
+
+    // Kết hợp với closure
+    let mut numbers: Vec<i32> = vec![1, 2, 3];
+    numbers.iter_mut().for_each(|x| *x += 1);
+    println!("numbers: {:?}", numbers);
+
+    let numbers1: Vec<i32> = vec![1, 2, 3];
+    let res: Vec<i32> = numbers1.iter().map(|x| x + 1).collect();
+    println!("res: {:?}", res);
+
+    let numbers2: Vec<i32> = vec![3, 4, 5];
+    let mut res = Vec::new();
+    for numb in numbers2.iter() {
+        res.push(numb + 1);
+    }
+    println!("res of numbers2: {:?}", res);
+
+    let mut numbers3: Vec<i32> = vec![4, 5, 6];
+    for numb in numbers3.iter_mut() {
+        *numb += 1;
+    }
+    println!("res of numbers3: {:?}", numbers3);
 }
